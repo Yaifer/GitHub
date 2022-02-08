9a77414b1be28fde2c17b81709305bc9df163f5a
Yaifer <JHANNON@madisoncollege.edu>
2022-02-07 21:08:54 -0600

    Initial commit for project 2
    Init for project 2

--- 2022-spring-adv-cloud-online/amplify-app/src/App.js
+++ 2022-spring-adv-cloud-online/amplify-app/src/App.js
@@ -6,8 +6,33 @@ import { withAuthenticator,
 const App = () => {
   return (
     <div>
-      <h1>Hello from AWS Amplify</h1>
+      <h1>Chill Chest 1.0</h1>
       <AmplifySignOut />
+      <h2>
+        This is just the begining!!!
+      </h2>
+      <fieldset>
+      <table>
+        <tr>
+          <td>
+            Hello
+          </td>
+          <td>
+            This is a test table
+          </td>
+        </tr>
+        <tr>
+          <td>
+            Not sure
+          </td>
+          <td>
+            what will happen  
+          </td>      
+        </tr>
+
+      </table>
+      </fieldset>
+      
     </div>
   );
 };
